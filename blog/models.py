c99b0d73f833b4398faf7a9248e2072e446bb5c0
EgorGologuzov <gologuzovegor@gmail.com>
2025-03-20 14:57:50 +0500

    Add image model? add gitignore

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,13 @@ from utils.utils import remove_html_tags
 
 class Article(models.Model):
   id = models.AutoField(primary_key=True)
-  position = models.IntegerField(null=True)
-  create_at = models.DateTimeField(null=False)
+  position = models.IntegerField(blank=True, default=0)
+  create_at = models.DateTimeField(auto_now_add=True, blank=True, null=False)
   title = models.CharField(max_length=150, null=False, default="[Title not set]")
   category_list_json = models.CharField(max_length=1000, null=False, default="[]")
+  avatar = models.CharField(max_length=200, blank=True, null=True, default=None)
   content_html = models.CharField(max_length=10_000_000, null=False, default="<p>[Content not set]</p>")
+  is_visible = models.BooleanField(null=False, default=True)
 
   def __str__(self):
     return f"[{self.id}] {self.title} ({self.create_at})"
@@ -19,11 +21,19 @@ class Article(models.Model):
 
 class Comment(models.Model):
   article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
-  send_at = models.DateTimeField(null=False)
+  send_at = models.DateTimeField(auto_now_add=True, blank=True, null=False)
   sender_name = models.CharField(max_length=150, null=False, default="[Anonim]")
   text = models.CharField(max_length=1000, null=False, default="[Text not set]")
   is_visible = models.BooleanField(null=False, default=True)
 
   def __str__(self):
     return f"{self.sender_name}: {self.text}"
+  
+
+class Image(models.Model):
+  name = models.CharField(max_length=200, blank=True, null=True)
+  file = models.ImageField(upload_to='images', null=False)
+
+  def __str__(self):
+    return f"{self.name} ({self.file.url})"
 
